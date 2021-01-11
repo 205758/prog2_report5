e3b385bdc35839e3f606fd28cbafb6b716eb2dbd
GiboRyouya <e205758@GiboRyouyanoMacBook-Air.local>
2021-01-11 22:33:15 +0900

    report5 Step3

--- app/src/main/java/Report5/Main.java
+++ app/src/main/java/Report5/Main.java
@@ -2,15 +2,8 @@ package Report5;
 
 public class Main {
     public static void main (String[] args){
-        String str = null;
-
-        try{
-            System.out.println(str.length());
-
-        }catch(NullPointerException e){
-            System.out.println("例外NullPointerExceptionが発生");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
     
 }
